5bd0355800f7a06f29b230edb3ccab8a5bed9c22
farzin-f <f.faridfar@gmail.com>
2015-04-20 21:11:31 -0400

    Finale - quinte still does'nt work

--- tp2.js
+++ tp2.js
@@ -1,3 +1,11 @@
+//Fichier: tp2.js
+//Auteur:  Melodie Diêu Thanh Verroeulst et Farzin Faridfar
+//IFT1015 - Travail Pratique #2: Jeu de Poker Shuffle - Avril 2015
+
+//Le Bogue: 
+//Les fonctions Quinte et Quint Flush ne fonctionnent pas
+//avec l'as au début de la séquence.
+
 function init(){
 
      document.getElementById("b").innerHTML = 
@@ -63,7 +71,7 @@ function init(){
 	return grille;
     };
 
-
+banqCarte = 0;
 };
 
 //Dans cette partie du code on crée un paquet de carte mélagée
@@ -87,43 +95,53 @@ function carte(carteCode){
     var coul = carteCoul[Math.floor(carteCode%4)];
     return val+coul;
 };
+/*function testCarte(){
+    assert(carte("2") == "2H");
+    assert(carte("24") == "8C");
+    assert(carte("41") == "QD");
+    assert(carte("51") == "AS");
+};
+testCarte();*/
 
 //idPrec: id de la carte précédent
 //banqCart: nombre de carte suivent dans le paquet    
 var idPrec = 0;
-var banqCart = 0;
+var banqCarte = 0;
 
 //Fonction clic(id): le corps du code qui fait un action selon le id
 //qu'elle reçoit après que l'utilisateur clique sur une carte
 function clic(id){
     
-    //Cette partie distribue les cartes de paquet sur les endroits
-    //vides de la grille
+    //Ici chaqu'une des cartes du paquet s'affiche
     if (id == 53){
 	document.getElementById(id).style.backgroundColor="lime";
-	document.getElementById(idPrec).style.backgroundColor=
-	    "transparent";
-	document.getElementById(id).innerHTML = 
-	    "<img src=\"cards/" + carte(paquet[banqCart]) + ".svg\">";
+	document.getElementById(53).innerHTML = 
+	    "<img src=\"cards/" + carte(paquet[banqCarte]) + ".svg\">";
 	
-	banqCart += 1;
+	banqCarte += 1;
 
+	//Pour désactiver la fonction onClick sur le paquet
+	//après qu'il soit exécuté au début du jeu pour afficher
+	//la première carte
+	document.getElementById(53).onclick = null;
     }
 
-    //Pour mettre les cartes dans la grille
+    //Cette partie distribue les cartes de paquet sur les endroits
+    //vides de la grille
     else if(document.getElementById(id).innerHTML ==
 	    "<img src=\"cards/empty.svg\">" && 
 	    document.getElementById(idPrec).style.backgroundColor
-	     !="lime"){
+	     !="lime" && document.getElementById(53).style.backgroundColor
+	     =="lime"){
 	document.getElementById(53).style.backgroundColor=
-	    "transparent";
+	    "lime";
 	document.getElementById(id).innerHTML = 
 	document.getElementById(53).innerHTML;
-	document.getElementById(id).value = paquet[banqCart-1];
+	document.getElementById(id).value = paquet[banqCarte-1];
 	document.getElementById(53).innerHTML = 
-	    "<img src=\"cards/" + carte(paquet[banqCart]) + ".svg\">";
+	    "<img src=\"cards/" + carte(paquet[banqCarte]) + ".svg\">";
 	idPrec = id;
-	banqCart += 1;
+	banqCarte += 1;
 	
 	calculPoints();
     }
@@ -136,9 +154,11 @@ function clic(id){
 	     !="lime" && document.getElementById(id).innerHTML !=
 	    "<img src=\"cards/empty.svg\">"
 	    ){
-	document.getElementById(id).style.backgroundColor="lime";
-	idPrec = id;
-	}
+		document.getElementById(53).style.backgroundColor=
+		    "transparent";
+		document.getElementById(id).style.backgroundColor="lime";
+		idPrec = id;
+	    }
 
     //La deuxième boucle, changer la carte qui est déjà sélectionnée
     //(la carte avec le background en lime)
@@ -146,11 +166,16 @@ function clic(id){
 	     =="lime"){
 	document.getElementById(idPrec).style.backgroundColor =
 	     "transparent";
+	document.getElementById(53).style.backgroundColor =
+	    "lime";
+
+	//Changement des cartes séléctionnés
 	var tempCarte = document.getElementById(id).innerHTML;
 	document.getElementById(id).innerHTML = 
 	document.getElementById(idPrec).innerHTML;
 	document.getElementById(idPrec).innerHTML = tempCarte;
 	
+	//Changement des valuers HTML de cartes séléctionnés
 	var tempVal = document.getElementById(id).value;
 	document.getElementById(id).value = 
 	document.getElementById(idPrec).value;
@@ -159,12 +184,14 @@ function clic(id){
 	idPrec = id;
 	calculPoints();
      }
+
     //Cette condition déclare la fin du jeu avec les points gangés
-    if (banqCart == 26){
+    if (banqCarte == 26){
 	alert("Votre pointage final est " + 
 	      document.getElementById(35).innerHTML);
 	init();
     }
+
 };
 
 
@@ -226,17 +253,52 @@ function calculPoints(){
 
 	if (document.getElementById(posV+30).innerHTML != ""){
 	    pointTotalV = pointTotalV + +document.getElementById(posV+30).innerHTML;
-	}
-
-	        
+	} 
     }
-
-
 
     document.getElementById(35).innerHTML = pointTotalH + pointTotalV;
 
 };
 
+//Fonction rangeResultat(tab): calculer le résultat de chaque
+//rangée et retourner le point le plus grand ou 'vide' si le point
+//est égal à zéro
+function rangeResultat(tab){
+    var resultatH = [];
+    resultatH.push(paireDlPaire(tabCarteH));
+    resultatH.push(brelan(tabCarteH));
+    resultatH.push(quinte(tabCarteH));
+    resultatH.push(flush(tabCarteH));
+    resultatH.push(fullHouse(tabCarteH));
+    resultatH.push(carre(tabCarteH));
+    resultatH.push(quinteFlush(tabCarteH));
+    resultatH.push(quinteFlushRoyale(tabCarteH));
+    
+    resultatH = trier(resultatH);
+
+    if(resultatH[resultatH.length-1] == 0){
+	   return "";
+    }else{
+	return resultatH[resultatH.length-1];
+    }
+};
+/*function testrangeResultat(){
+    assert(rangeResultat([0,4,16,28,48]) == "");
+    assert(rangeResultat([15,23,32,33,40]) == "2");
+    assert(rangeResultat([16,17,32,33,40]) == "5");
+    assert(rangeResultat([16,17,18,33,40]) == "10");
+    assert(rangeResultat([16,20,25,30,32]) == "15");
+    assert(rangeResultat([0,4,16,28,48]) == "20");
+    assert(rangeResultat([1,3,41,42,43]) == "25");
+    assert(rangeResultat([16,17,18,19,40]) == "50");
+    assert(rangeResultat([3,7,11,15,19]) == "75");
+    assert(rangeResultat([32,36,40,44,48]) == "100");
+};
+testColloneResultat();*/
+
+//Fonction colloneResultat(tab): calculer le résultat de chaque
+//collone et retourner le point le plus grand ou 'vide' si le point
+//est égal à zéro
 function colloneResultat(tab){
     var resultatV = [];
     resultatV.push(paireDlPaire(tabCarteV));
@@ -257,28 +319,22 @@ function colloneResultat(tab){
     }
 };
 
-
-function rangeResultat(tab){
-    var resultatH = [];
-    resultatH.push(paireDlPaire(tabCarteH));
-    resultatH.push(brelan(tabCarteH));
-    resultatH.push(quinte(tabCarteH));
-    resultatH.push(flush(tabCarteH));
-    resultatH.push(fullHouse(tabCarteH));
-    resultatH.push(carre(tabCarteH));
-    resultatH.push(quinteFlush(tabCarteH));
-    resultatH.push(quinteFlushRoyale(tabCarteH));
-    
-    resultatH = trier(resultatH);
-
-    if(resultatH[resultatH.length-1] == 0){
-	   return "";
-    }else{
-	return resultatH[resultatH.length-1];
-    }
+/*function testColloneResultat(){
+    assert(colloneResultat([0,4,16,28,48]) == "");
+    assert(colloneResultat([15,23,32,33,40]) == "2");
+    assert(colloneResultat([16,17,32,33,40]) == "5");
+    assert(colloneResultat([16,17,18,33,40]) == "10");
+    assert(colloneResultat([16,20,25,30,32]) == "15");
+    assert(colloneResultat([0,4,16,28,48]) == "20");
+    assert(colloneResultat([1,3,41,42,43]) == "25");
+    assert(colloneResultat([16,17,18,19,40]) == "50");
+    assert(colloneResultat([3,7,11,15,19]) == "75");
+    assert(colloneResultat([32,36,40,44,48]) == "100");
 };
+testColloneResultat();*/
 
-
+//Fonction paireDlPaire(tab): une paire de meme valeur
+//ou deux paires chaqu'une de deux valeurs différentes
 function  paireDlPaire(tab){
     tab = tabCarteVal(tab);
     var tabFreq = freqElem(tab);
@@ -297,9 +353,15 @@ function  paireDlPaire(tab){
 	return 5;
     }
 return 0;  
-
 };
+/*function testPaireDlPaire(){
+    assert(paireDlPaire([0,4,16,28,48]) == "0");
+    assert(paireDlPaire([15,23,32,33,40]) == "2");
+    assert(paireDlPaire([16,17,32,33,40]) == "5");
+};
+testPaireDlPaire();*/
 
+//Fonction brelan(tab): trois cartes de meme valeur
 function  brelan(tab){
     tab = tabCarteVal(tab);
     var tabFreq = freqElem(tab);
@@ -313,7 +375,15 @@ function  brelan(tab){
     return 0;
 
 };
+/*function testBrelan(){
+    assert(brelan([0,4,16,28,48]) == "0");
+    assert(brelan([15,23,32,33,40]) == "0");
+    assert(brelan([16,17,32,33,40]) == "0");
+    assert(brelan([16,17,18,33,40]) == "10");
+};
+testBrelan();*/
 
+//Fonction quinte(tab): cinq cartes qui suivent
 function quinte(tab){
     if (tab.length < 5){
 	return 0;
@@ -327,7 +397,17 @@ function quinte(tab){
     }
 return 15;
 };
+/*functiotun testquinte(){
+    assert(quinte([0,4,16,28,48]) == "0");
+    assert(quinte([16,20,25,30,32]) == "15");
+    //l'as à la fin
+    assert(quinte([32,37,40,47,50]) == "15");
+    //l'as au début (il ne fonctionne pas!!)
+    assert(quinte([1,5,8,13,50]) == "15");
+};
+testquinte();*/
 
+//Fonction flush(tab): toutes les cartes de meme couleurs
 function flush(tab){
 
     if (tab.length < 5){
@@ -342,7 +422,15 @@ function flush(tab){
 	return 20;
     }
 };
+/*function testFlush(){
+    assert(flush([0,4,16,28,48]) == "20");
+    assert(flush([16,20,25,30,32]) == "0");
+    assert(flush([16,17,18,33,40]) == "0");
+};
+testFlush();*/
 
+//Fonctio fullHouse(tab): trois cartes de meme valeur et
+// une paire de meme valeur
 function  fullHouse(tab){
     if (tab.length < 5){
 	return 0;
@@ -357,10 +445,16 @@ function  fullHouse(tab){
 	}
     }
     return 0;
-
 };
+/*function testFullHouse(){
+    assert(fullHouse([0,4,16,28,48]) == "0");
+    assert(fullHouse([1,5,8,13,50]) == "0");
+    assert(fullHouse([1,3,41,42,43]) == "25");
+};
+testFullHouse();*/
 
-function  carre(tab){
+//Fonction carre(tab): quatre cartes de meme valeur
+function carre(tab){
     tab = tabCarteVal(tab);
     var tabFreq = freqElem(tab);
 
@@ -372,21 +466,47 @@ function  carre(tab){
     
     return 0;  
 };
+/*function testCarre(){
+    assert(carre([0,4,16,28,48]) == "0");
+    assert(carre([16,17,32,33,40]) == "0");
+    assert(carre([16,17,18,33,40]) == "0");
+    assert(carre([16,17,18,19,40]) == "50");
+};
+testCarre();*/
 
-
+//Fonctio quinteFlush(tab): cinq cartes de meme couleur qui suivent
 function quinteFlush(tab){
     if(flush(tab) == 20 && quinte(tab) == 15){
 	return 75;	
     }
 return 0;
 };
+/*function testQuinteFlush(){
+    assert(testQuinteFlush([0,4,16,28,48]) == "0");
+    assert(testQuinteFlush([16,20,25,30,32]) == "0");
+    assert(testQuinteFlush([32,37,40,47,50]) == "0");
+    //l'as au début (il ne fonctionne pas!!)
+    assert(testQuinteFlush([1,5,8,13,50]) == "0");
+    assert(testQuinteFlush([3,7,11,15,19]) == "75");
+};
+testQuinteFlush();*/
 
+//Fonctio quinteFlushRoyale(tab): l'as, le roi, la dame le valet
+// et le 10 d'une meme couleur
 function quinteFlushRoyale(tab){
-    if(quinteFlush(tab) == 75 && (tab[0]>>2) == 9){
+    if(quinteFlush(tab) == 75 && (tab[4]>>2) == 9){
 	return 100;	
     }
 return 0;
 };
+/*function testQuinteFlushRoyale(){
+    assert(testQuinteFlushRoyale([0,4,16,28,48]) == "0");
+    assert(testQuinteFlushRoyale([32,37,40,47,50]) == "0");
+    assert(testQuinteFlushRoyale([3,7,11,15,19]) == "0");
+    assert(testQuinteFlushRoyale([32,36,40,44,48]) == "100");
+};
+testQuinteFlushRoyale();*/
+
 
 //Fonction trier(tab): prend un tab et trie les valeur
 //en ordre croissant
@@ -406,6 +526,13 @@ function trier(tab) {
     } while (echange);
     return tab;
 };
+/*function testTrier(){
+    assert(trier([0,16,4,48,28]) == "0,4,16,28,48");
+    assert(trier([29,13,33,9,45]) == "9,13,29,33,45");
+    assert(trier([10,2,20,5,75]) == "2,5,10,20,75");
+};
+testTrier();*/
+
 
 //Fonction tabCarteCoul(tab): calculer les couleurs des cartes
 //dans le tableau "tab" qui contient les codes de cartes
@@ -416,6 +543,12 @@ function tabCarteCoul(tab){
     }
     return tabTemp;
 };
+/*function testTabCarteCoul(){
+    assert(tabCarteCoul([0,16,4,48,28]) == "0,0,0,0,0");
+    assert(tabCarteCoul([37,36,4,33,28]) == "1,0,0,1,0");
+    assert(tabCarteCoul([6,7,8,9,28]) == "2,3,0,1,0");
+};
+testTabCarteCoul();*/
 
 //Fonction tabCarteVal(tab): calculer les valeurs des cartes
 //dans le tableau "tab" qui contient les codes de cartes
@@ -426,6 +559,13 @@ function tabCarteVal(tab){
     }
     return tabTemp;
 };
+/*function testTabCarteVal(){ 
+    assert(tabCarteVal([0,16,4,48,28]) == "0,4,1,12,7");
+    assert(tabCarteVal([37,36,4,38,28]) == "9,9,1,9,7");
+    assert(tabCarteVal([17,16,4,31,28]) == "4,4,1,7,7");
+    assert(tabCarteVal([4,8,12,16,20]) == "1,2,3,4,5");
+};
+testTabCarteVal();*/
 
 //Fonction ferquElem(tab): elle prend un tableau et returne
 //la fréquence de chaque élément du tableau
@@ -445,3 +585,4 @@ function freqElem(tab){
     return resultat;
 };
 
+var assert = require('assert');
